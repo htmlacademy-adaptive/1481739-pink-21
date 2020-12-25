0df4eae7ca72c2b34b61e943620a74451238efbb
olga2102 <70343745+olga2102@users.noreply.github.com>
2020-12-26 00:03:28 +0300

    Правки меню, движение в потоке

--- source/js/menu.js
+++ source/js/menu.js
@@ -2,11 +2,12 @@ const menu = document.querySelector('.main-nav__list');
 const openMenu = document.querySelector('.main-nav__toggle');
 const background = document.querySelector('.page-header__container');
 const openButton = document.querySelector('.main-nav__toggle');
+const picture = document.querySelector('.page-header');
 
 openMenu.addEventListener('click', function () {
   menu.classList.toggle('main-nav__list--open');
   background.classList.toggle('page-header__container--bg');
   openButton.classList.toggle('main-nav__toggle--close');
   openButton.classList.toggle('main-nav__toggle--open');
-
+  picture.classList.toggle('page-header--bg');
 });
