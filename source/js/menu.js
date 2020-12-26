2676316b91f1de941d34abcf9536d1b32969a766
olga2102 <70343745+olga2102@users.noreply.github.com>
2020-12-26 22:21:27 +0300

    поправки по бэму

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,13 +1,13 @@
 const menu = document.querySelector('.main-nav__list');
-const openMenu = document.querySelector('.main-nav__toggle');
+const openMenu = document.querySelector('.toggle');
 const background = document.querySelector('.page-header__container');
-const openButton = document.querySelector('.main-nav__toggle');
+const openButton = document.querySelector('.toggle');
 const picture = document.querySelector('.page-header');
 
 openMenu.addEventListener('click', function () {
   menu.classList.toggle('main-nav__list--open');
   background.classList.toggle('page-header__container--bg');
-  openButton.classList.toggle('main-nav__toggle--close');
-  openButton.classList.toggle('main-nav__toggle--open');
+  openButton.classList.toggle('toggle--close');
+  openButton.classList.toggle('toggle--open');
   picture.classList.toggle('page-header--bg');
 });
