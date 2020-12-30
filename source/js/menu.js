0b021e9c85da2f18145527b15be503d0c9edb177
olga2102 <70343745+olga2102@users.noreply.github.com>
2020-12-30 21:51:43 +0300

    Сборка

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,8 +1,8 @@
-const menu = document.querySelector('.main-nav__list');
-const openMenu = document.querySelector('.toggle');
-const background = document.querySelector('.page-header__container');
-const openButton = document.querySelector('.toggle');
-const picture = document.querySelector('.page-header');
+var menu = document.querySelector('.main-nav__list');
+var openMenu = document.querySelector('.toggle');
+var background = document.querySelector('.page-header__container');
+var openButton = document.querySelector('.toggle');
+var picture = document.querySelector('.page-header');
 
 openMenu.addEventListener('click', function () {
   menu.classList.toggle('main-nav__list--open');
